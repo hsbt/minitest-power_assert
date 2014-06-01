7b6ff8992a7f7ce7403d2d60350e4269ca58e5e6
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2014-06-01 16:29:33 +0900

    workaround for minitest-reporters

--- lib/minitest/power_assert.rb
+++ lib/minitest/power_assert.rb
@@ -9,7 +9,8 @@ module Minitest
       def assert test = nil, msg = nil, &blk
         if block_given?
           ::PowerAssert.start(blk, assertion_method: __method__) do |pa|
-            super pa.yield, pa.message_proc
+            # XXX workaround to break inline format with minitest-reporters.
+            super pa.yield, "\n#{pa.message_proc.call}"
           end
         else
           super test, msg
