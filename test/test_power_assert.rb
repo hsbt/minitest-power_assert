7314e87adeb0c9aed600b7509aeacd4a4659ad39
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2014-06-01 15:12:18 +0900

    added fail testcase

--- test/test_power_assert.rb
+++ test/test_power_assert.rb
@@ -1,5 +1,6 @@
 require "minitest/autorun"
 require 'minitest/power_assert'
+require 'stringio'
 
 class TestPowerAssert < Minitest::Test
   def test_power_assert
@@ -8,6 +9,11 @@ class TestPowerAssert < Minitest::Test
 
   def test_power_assert_failed
     assert { "0".class == "3".to_i.times.map {|i| i + 1 }.class }
+  rescue Minitest::Assertion => e
+    assert_match(/Array/, e.message)
+    assert_match(/\[1, 2, 3\]/, e.message)
+    assert_match(/#<Enumerator: 3:times>/, e.message)
+    assert_match(/String/, e.message)
   end
 
   def test_assert
@@ -16,5 +22,7 @@ class TestPowerAssert < Minitest::Test
 
   def test_assert_failed
     assert "0".class == "3".to_i.times.map {|i| i + 1 }.class
+  rescue Minitest::Assertion => e
+    assert_match(/Failed assertion, no message given./, e.message)
   end
 end
