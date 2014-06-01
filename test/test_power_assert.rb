88f88919545cc8e84a1b0fc9b98d2197ec622796
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2014-06-01 14:54:30 +0900

    style

--- test/test_power_assert.rb
+++ test/test_power_assert.rb
@@ -3,14 +3,10 @@ require 'minitest/power_assert'
 
 class TestPowerAssert < Minitest::Test
   def test_successful
-    power_assert do
-      true
-    end
+    power_assert { true }
   end
 
   def test_failed
-    power_assert do
-      "0".class == "3".to_i.times.map {|i| i + 1 }.class
-    end
+    power_assert { "0".class == "3".to_i.times.map {|i| i + 1 }.class }
   end
 end
