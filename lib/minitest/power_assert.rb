688cb8f15b549b8f709e87fe34b3d75b40ec4f17
Mike Blumtritt <mike.blumtritt@injixo.com>
2015-04-29 15:15:35 +0200

    repair refute

--- lib/minitest/power_assert.rb
+++ lib/minitest/power_assert.rb
@@ -6,27 +6,34 @@ require 'power_assert'
 module Minitest
   module PowerAssert
     module Assertions
-      def assert test = nil, msg = nil, &blk
+      def assert(test = nil, msg = nil)
         if block_given?
-          ::PowerAssert.start(blk, assertion_method: __method__) do |pa|
-            # XXX workaround to break inline format with minitest-reporters.
-            super pa.yield, "\n#{pa.message_proc.call}"
+          ::PowerAssert.start(Proc.new, assertion_method: __method__) do |pa|
+            super pa.yield, pa.extend(ContextExtension)
           end
         else
-          super test, msg
+          super
         end
       end
 
-      def refute test = nil, msg = nil, &blk
+      def refute(test = nil, msg = nil)
         if block_given?
-          ::PowerAssert.start(blk, assertion_method: __method__) do |pa|
-            # XXX workaround to break inline format with minitest-reporters.
-            super pa.yield, "\n#{pa.message_proc.call}"
+          ::PowerAssert.start(Proc.new, assertion_method: __method__) do |pa|
+            super pa.yield, pa.extend(ContextExtension)
           end
         else
-          super test, msg
+          super
         end
       end
+
+
+      module ContextExtension
+        def to_s
+          message
+        end
+      end
+      private_constant :ContextExtension
+
     end
   end
 
